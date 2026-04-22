e6fa446db42c4ab6255ac05e331277324058e347
darshan-gowdaa <darshangowdaa223@gmail.com>
2026-04-22 12:33:45 +0530

    bug fix: remove hardcoded localhost from apiutils axios config

--- frontend/src/utils/apiUtils.js
+++ frontend/src/utils/apiUtils.js
@@ -3,7 +3,8 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "/api";
+axios.defaults.baseURL = API_BASE_URL;
 axios.defaults.timeout = 10000;
 
 axios.interceptors.response.use(
