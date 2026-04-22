32d502b41a87d85d6f029d2bec2026b3af5c74e5
darshan-gowdaa <darshangowdaa223@gmail.com>
2026-04-22 12:36:32 +0530

    bug fix: attach jwt token to global axios requests in apiutils to resolve 401 errors

--- frontend/src/utils/apiUtils.js
+++ frontend/src/utils/apiUtils.js
@@ -2,10 +2,20 @@
 
 import axios from "axios";
 import { toast } from "react-toastify";
+import { STORAGE_KEYS } from "../constants/constants";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || "/api";
 axios.defaults.baseURL = API_BASE_URL;
 axios.defaults.timeout = 10000;
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(STORAGE_KEYS.TOKEN);
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 axios.interceptors.response.use(
   (res) => res,
